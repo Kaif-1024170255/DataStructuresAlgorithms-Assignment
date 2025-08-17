3ce6b195e66de17c6b4cc3cb66bdec37411d297d
Kaif-1024170255 <valorantkaif01@gmail.com>
2025-08-17 16:10:24 +0530

    1q

--- Assignment-One/Ques1.cpp
+++ Assignment-One/Ques1.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
 using namespace std;
+
 int main(){
+    int ch;
+    cout<<"Enter Choice : \n"<<"1.Create\n"<<"2.Display\n"<<"3.Insert\n"<<"4.Delete\n"<<"5.Linear Search"<<"*.Exit"<<endl;
+    cin>>ch;
+    while(ch<=5){
+        switch (ch){
+        case 1:
+            //Create
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            break;
+        }
+    }  
     return 0;
 }
